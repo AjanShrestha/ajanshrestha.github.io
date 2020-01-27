da5791df42159ec68ec024ad4eccd7904ebe39d3
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-01-28 10:47:32 +1100

    feat: Used node-fetch to handle http requests

--- playground/github.js
+++ playground/github.js
@@ -1,62 +1,64 @@
-const https = require('https');
 const fs = require('fs');
 
-const generateOptions = type => {
-  const options = {
-    host: 'api.github.com',
-    path: '/repos/AjanShrestha',
-    method: 'get',
-    headers: {
-      'user-agent': 'AjanShrestha',
-      Accept: 'application/json',
-    },
-  };
+const fetch = require('node-fetch');
 
-  switch (type) {
-    case 'repos': {
-      options.path = `${options.path}/repos?type=all&sort=updated`;
-      return options;
-    }
-    case 'topics': {
-      options.path = `${options.path}/${repoName}/topics`;
-      options.headers.Accept = 'application/vnd.github.mercy-preview+json';
-      return options;
-    }
-    default:
-      return options;
-  }
+const USERNAME = 'AjanShrestha';
+
+const options = {
+  method: 'get',
+  headers: {
+    'user-agent': 'AjanShrestha',
+    Accept: 'application/json',
+  },
 };
 
-https.get(options, res => {
-  res.setEncoding('utf8');
-  let body = '';
-  res.on('data', data => {
-    body += data;
-  });
-  res.on('end', () => {
-    // body = body);
-    console.log(body);
-  });
-});
+const fetchTopics = repoName => {
+  const topicUrl = `https://api.github.com/users/${USERNAME}/${repoName}/topics`;
+  options.headers.Accept = 'application/vnd.github.mercy-preview+json';
+  fetch();
+};
 
-// let allRepos = [];
-// for (let i = 1; i < 4; i++) {
-//   options.path = `${options.path}&page=${i}`;
-//   https.get(options, res => {
-//     res.setEncoding('utf8');
-//     let body = '';
-//     res.on('data', data => {
-//       body += data;
-//     });
-//     res.on('end', () => {
-//       body = JSON.parse(body);
-//       allRepos = [...allRepos, ...body];
-//       fs.writeFileSync('playground/repos.json', JSON.stringify(allRepos));
-//     });
-//   });
-// }
+const getAllRepos = async () => {
+  let allRepos = [];
+  let pageNum = 1;
+  while (pageNum != 0) {
+    await fetch(
+      `https://api.github.com/users/${USERNAME}/repos?type=all&sort=updated&page=${pageNum}`,
+      options
+    )
+      .then(res => res.json())
+      .then(jsonRes => {
+        console.log(jsonRes.length);
+        if (jsonRes.length) {
+          allRepos = [...allRepos, ...jsonRes];
+          pageNum++;
+        } else {
+          pageNum = 0;
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        pageNum = 0;
+      });
+  }
+  return allRepos;
+};
 
-// const repos = JSON.parse(fs.readFileSync('playground/repos.json', 'utf-8'));
+const writeAllRepos = repos =>
+  fs.writeFileSync('playground/allRepos.json', JSON.stringify(repos));
+
+(async () => {
+  console.group('repos');
+  // let allRepos = await getAllRepos();
+  // writeAllRepos(allRepos);
+  const repos = JSON.parse(
+    fs.readFileSync('playground/allRepos.json', 'utf-8')
+  );
+  console.log(repos.length);
+  console.groupEnd('repos');
+})();
+
+// const repos = JSON.parse(fs.readFileSync('playground/allRepos.json', 'utf-8'));
 // console.log(repos.length);
 // const forked = repos.filter(repo => repo.fork);
 // console.log(`Forked: ${forked.length}`);
